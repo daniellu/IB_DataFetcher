700e9d85eeacae7f9871ea6ca2e6a6422b7a3d4e
zlu <luzhanxun.daniel@gmail.com>
2016-03-29 21:42:03 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IBApi;
+
+namespace IB.PriceFetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EWrapperImpl client = new EWrapperImpl();
+
+            //connect
+            client.ClientSocket.eConnect("127.0.0.1", 7496, 0);
+
+            Console.WriteLine("Application finishes running, press enter to exist...");
+            //Console.ReadLine();
+        }
+    }
+}
