1292e179c9c007ccd1387f6c869754cd740b4ac8
zlu <luzhanxun.daniel@gmail.com>
2016-04-03 21:05:58 -0700

    separate csv export

---
+++ CsvFileGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using CsvHelper;
+
+namespace IB.PriceFetcher
+{
+    public class CsvFileGenerator
+    {
+        public void GenerateCsvFile(int requestId, string folderName, IEnumerable<HistoricalDataMessage> historicalData)
+        {
+            var filePath = Path.Combine(folderName, requestId + ".csv");
+            using (var textWriter = File.CreateText(filePath))
+            {
+                var csv = new CsvWriter(textWriter);
+                csv.WriteRecords(historicalData);
+            }
+        }
+    }
+}
