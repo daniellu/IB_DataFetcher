0eac32135bdcc484e688628f9ed47df0aa04e39b
Daniel <luzhanxun.daniel@gmail.com>
2016-04-12 09:40:55 -0700

    add command line parameters support

---
+++ Options.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace IB.PriceFetcher
+{
+    public class Options
+    {
+        [Option('s', "symbol", Required = true, HelpText = "Input stock symbo to process.")]
+        public string Symbol { get; set; }
+
+        [Option('e', "end-date", Required = true, HelpText = "Input end date to process. Format: yyyyMMdd HH:mm:ss. Example: 20131009 23:59:59")]
+        public string EndDate { get; set; }
+
+        [Option('d', "duration", Required = true, HelpText = "Input duration to process. Duration unit: S, D, W, M, Y")]
+        public string Duration { get; set; }
+
+        [Option('b', "bar-size", Required = false, DefaultValue = "1 min", HelpText = "Input duration to process. Valid bar size: 1 sec, 5 secs, 15 secs, 30 secs, 1 min, 2 mins, 3 mins, 5 mins, 15 mins, 30 mins, 1 hour, 1 day")]
+        public string BarSize { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,23 @@ namespace IB.PriceFetcher
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // Values are available here
+                Console.WriteLine("Stock Symbol: {0}", options.Symbol);
+                Console.WriteLine("End Date: {0}", options.EndDate);
+                Console.WriteLine("Duration: {0}", options.Duration);
+                Console.WriteLine("Bar Size: {0}", options.BarSize);
+            }
+            else
+            {
+                //Console.WriteLine(options.GetUsage());
+                Console.WriteLine("Press enter to exist...");
+                Console.ReadLine();
+                return;
+            }
+
             int nextRequestId = 1;
             var priceDataManager = new PriceDataManager();
             EWrapperImpl client = new EWrapperImpl(priceDataManager);
@@ -20,12 +37,12 @@ namespace IB.PriceFetcher
             //connect
             client.ClientSocket.eConnect("127.0.0.1", 7496, 0);
 
-            var stockContract = GetStockContract();
+            var stockContract = GetStockContract(options.Symbol);
             client.ClientSocket.reqContractDetails(nextRequestId++, stockContract);
 
             //nextRequestId is the request Id appears in the response
             //program uses this id to match where the request comes from 
-            client.ClientSocket.reqHistoricalData(nextRequestId++, stockContract, "20131009 23:59:59", "10 D", "1 min", "TRADES", 1, 1, null);
+            client.ClientSocket.reqHistoricalData(nextRequestId++, stockContract, options.EndDate, options.Duration, options.BarSize, "TRADES", 1, 1, null);
             Thread.Sleep(10000);
             Console.WriteLine("Disconnecting...");
             client.ClientSocket.eDisconnect();
@@ -34,10 +51,11 @@ namespace IB.PriceFetcher
             //Console.ReadLine();
         }
 
-        public static Contract GetStockContract()
+        public static Contract GetStockContract(string symbol)
         {
             Contract contract = new Contract();
-            contract.Symbol = "TRI";
+            //contract.Symbol = "TRI";
+            contract.Symbol = symbol;
             contract.SecType = "STK";
             contract.Currency = "CAD";
             contract.Exchange = "SMART";
